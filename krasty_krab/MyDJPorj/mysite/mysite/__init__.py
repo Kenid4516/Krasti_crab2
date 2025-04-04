fe45ec969a17b7bd261226a557026e08ac4fd115
Mazda-prokuratura <lapsinegor58@gmail.com>
2025-04-04 09:30:34 +0700

    Delete krasty_krab directory

--- krasty_krab/MyDJPorj/mysite/mysite/__init__.py
+++
@@ -1,5 +0,0 @@
-# from .user_models import User
-# from .menu_models import MenuItem
-# from .cart_models import Cart, CartItem
-# from .order_models import Order
-# from .delivery_models import Delivery
--- krasty_krab/orders/models/user_models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class User(AbstractUser):
-    phone = models.CharField(max_length=15, unique=True)
-    address = models.TextField()
